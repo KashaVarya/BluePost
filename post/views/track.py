d20ba1069e7c4d032765c7f10433f6690ac5b899
Kristobal <junta.kristobal@gmail.com>
2019-12-25 00:28:26 +0200

    implement tracking functionality

--- post/views/track.py
+++ post/views/track.py
@@ -1,5 +1,9 @@
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
 from django.shortcuts import redirect
+from django.http import Http404
+
+from post.models import Package
 
 
 class TrackView(TemplateView):
@@ -10,11 +14,15 @@ class TrackView(TemplateView):
         return context
 
     def post(self, request, *args, **kwargs):
-        return redirect('track_result', 42)
+        package = Package.objects.filter(tracking_number=request.POST.get('tracking_number')).first()
+        if not package:
+            raise Http404
+        return redirect('track_result', package.pk)
 
 
-class TrackResultView(TemplateView):
+class TrackResultView(DetailView):
     template_name = 'post/track_result.html'
+    model = Package
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
