69a8aa92cd530d16b207f89b8bb1e2bc86495f18
KashaVarya <vk@albedo-tech.com>
2019-12-25 18:29:31 +0200

    output all package fields

--- post/views/track.py
+++ post/views/track.py
@@ -2,6 +2,7 @@ from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
 from django.shortcuts import redirect
 from django.http import Http404
+from django.forms.models import model_to_dict
 
 from post.models import Package
 
@@ -26,4 +27,10 @@ class TrackResultView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        context['package'] = {}
+        context['package_attr'] = model_to_dict(self.object)
+        for k, v in context['package_attr'].items():
+            context['package'][k.title().replace('_', ' ')] = v
+
         return context
